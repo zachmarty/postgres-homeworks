5f2fb974e6070632c9059f260528b2a8e74ef176
Pavel <pashapasha-2013@mail.ru>
2023-10-27 16:44:43 +0300

    sql added

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+create table customers(
+customer_id varchar(10) primary key,
+company_name text,
+contact_name varchar(30)
+);
+
+create table employees(
+employee_id serial primary key,
+first_name varchar(10),
+last_name varchar(10),
+title varchar(30),
+birth_date date,
+notes text
+);
+
+create table orders(
+order_id serial primary key,
+customer_id varchar(10) not null,
+employee_id integer not null,
+order_date date,
+ship_city varchar(20)
+);
